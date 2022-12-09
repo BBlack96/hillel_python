362033933c4995955492c09caf450b98b8c2e085
BBlack96 <bogdan7450@gmail.com>
2022-12-09 13:12:16 +0200

    Commit with Homework4

---
+++ functions_file.py
@@ -0,0 +1,24 @@
+def unique_check(x):
+    numbers_values = []
+    str_values = []
+    if len(x) > 8 or len(x) < 8:
+        print("Ошибка! Вы ввели значение с некорректной длиной")
+        return False
+    for i in x:
+        if i.isdigit():
+            numbers_values.append(int(i))
+    print("Ваше значение содержит следующие цифры: ", numbers_values[0:5])
+
+    for i in x:
+        if i.isalpha():
+           str_values.append(str(i))
+    print("Ваше значение содержит следующие буквы: ", str_values[2:7])
+
+
+def summ(x):
+    new_list = []
+    for i in x:
+        if i.isdigit():
+            new_list.append(int(i))
+    print("Сумма всех чисел вашего значения равна: ", sum(new_list))
+    return sum(new_list)
---
+++ homework_4.py
@@ -0,0 +1,20 @@
+import json
+from functions_file import unique_check
+from functions_file import summ
+
+with open("list.json", "r") as f:
+    data_list = json.load(f)
+
+plates_list_1 = set(data_list)
+print("В вашем списке", len(plates_list_1), "уникальных значений")
+
+user_value = input("Введите ваше значение: ").upper()
+x = user_value
+plates_list_1 = set(data_list)
+if user_value in plates_list_1:
+    print("Такое значение уже есть в списке")
+else:
+    print("Введенное значение отсутствует в списке")
+
+unique_check(x)
+summ(x)
